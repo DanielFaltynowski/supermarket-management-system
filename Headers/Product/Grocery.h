84ddd88ef7709f6beec6484c26b167d2e0c29b5a
Daniel <faltynowskidaniel6@gmail.com>
2023-08-25 10:07:08 +0200

    Implementation of the Grocery class inheriting from the Product class.

---
+++ Headers/Product/Grocery.h
@@ -0,0 +1,41 @@
+#ifndef SUPERMARKET_MANAGEMENT_SYSTEM_GROCERY_H
+#define SUPERMARKET_MANAGEMENT_SYSTEM_GROCERY_H
+#include <iostream>
+
+using namespace std;
+
+#include "../Product.h"
+
+class Grocery : public Product
+{
+public:
+    // Constructors
+    Grocery()
+    {
+        this->kcal = 0;
+    }
+
+    Grocery(string name, double price, int kcal) : Product(name, price)
+    {
+        this->kcal = kcal;
+    }
+
+    // Getters
+    int getKcal()
+    {
+        return this->kcal;
+    }
+
+    // Setters
+    void setKcal(int kcal)
+    {
+        this->kcal = kcal;
+    }
+
+
+protected:
+    int kcal;
+
+};
+
+#endif //SUPERMARKET_MANAGEMENT_SYSTEM_GROCERY_H
